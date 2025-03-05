07d6df81815a92cbe648f43c23d7e2736172104e
Dickson <owusudicksonnyantakyi@gmail.com>
2025-03-05 08:56:48 +0800

    Uploading full MERN project

---
+++ frontend/src/Pages/homepage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function HomePage() {
+  return (
+    <div>
+      <h1>Welcome!</h1>
+      <p>This is a very basic home page.</p>
+    </div>
+  );
+}
+
+export default HomePage;
