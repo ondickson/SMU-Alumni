476fbd38184efdac5a6aac076c9b72e9700eb50c
kerty <briikzdev@gmail.com>
2025-03-06 08:55:39 +0800

    login fixed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './Pages/homepage';
+import Login from './Pages/login';
+import AlumniInformation from './Pages/AlumniPage/AlumniInformation';
+import StudentInformation from './Pages/AlumniPage/StudentInformation';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/AlumniInformation" element={<AlumniInformation />} />
+        <Route path="/StudentInformation" element={<StudentInformation />} />
       </Routes>
     </Router>
   );
---
+++ frontend/src/Pages/login.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./login.css";
+import logo from "../Assets/smulogo.png"; // Correctly import the image
+
+const Login = () => {
+  return (
+    <div className="login-container">
+      <div className="login-box">
+        <img src={logo} alt="SMU Logo" className="logo" />
+        <h2 className="title">SMU Alumni Tracer</h2>
+        <form>
+          <input type="text" placeholder="Username" className="input-field" />
+          <input type="password" placeholder="Password" className="input-field" />
+          <button type="submit" className="login-button">Login</button>
+        </form>
+        <p className="signup-text">
+          Don't have an account? <a href="/signup">Sign Up</a>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
