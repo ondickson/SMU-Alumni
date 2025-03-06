89ed2c9b1cf6662a72abc05a908fac895e83b33b
aubdacx <aubreighlee30@gmail.com>
2025-03-06 09:16:05 +0800

    routing of sidebar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,14 +4,16 @@ import HomePage from './Pages/homepage';
 import Login from './Pages/login';
 import AlumniInformation from './Pages/AlumniPage/AlumniInformation';
 import StudentInformation from './Pages/AlumniPage/StudentInformation';
+import AdminDashboard from './Pages/AdminPage/AdminDashboard';
 
 function App() {
   return (
     <Router>
-      <Routes>
+      <Routes>  
         <Route path="/" element={<Login />} />
         <Route path="/AlumniInformation" element={<AlumniInformation />} />
         <Route path="/StudentInformation" element={<StudentInformation />} />
+        <Route path="/AdminDashboard" element={<AdminDashboard />} />
       </Routes>
     </Router>
   );
